a4edc5ea2e6f85806316a121698954c2a26f819b
andreibtm <131543636+andreibtm@users.noreply.github.com>
2025-03-20 12:42:23 +0200

    Update rez2.cpp

--- var-2025/ex2/rez2.cpp
+++ var-2025/ex2/rez2.cpp
@@ -6,20 +6,20 @@ int main()
     int k, n;
     cin >> k >> n;
     int a[100][100];
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        int aux = i;
-        for (int j = 0; j <= n; j++)
+        int aux = i + 1;
+        for (int j = 0; j < n; j++)
         {
-            a[i][j] = k * i + aux;
+            a[i][j] = k * (i + 1) + aux - 1 ;
             aux--;
         }
     }
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
-            cout << a[i][j];
+        for (int j = 0; j < n; j++)
+            cout << a[i][j] << " ";
 
         cout << endl;
     }
